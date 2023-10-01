d999be96ffdef7d55746fa223380092ef911a0f3
duwaista <vlad.45.45@mail.ru>
2023-10-01 17:53:49 +0500

    init

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { NestApplicationOptions } from '@nestjs/common';
+import { AppModule } from './app.module';
+
+const appOptions: NestApplicationOptions = {
+  logger: ['log', 'error', 'warn'],
+};
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule, appOptions);
+
+  const config = new DocumentBuilder()
+    .setTitle('Test title')
+    .setDescription('Test description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(3000);
+}
+bootstrap();
